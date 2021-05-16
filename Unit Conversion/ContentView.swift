a5aa4213eb202f77ff04ff40090549e736647ce2
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-05-16 13:54:54 +0200

    Initial Commit

---
+++ Unit Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Conversion
+//
+//  Created by Mahmoud Fouad on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unit Conversion/Unit_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionApp.swift
+//  Unit Conversion
+//
+//  Created by Mahmoud Fouad on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
