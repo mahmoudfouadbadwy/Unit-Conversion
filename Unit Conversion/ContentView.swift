7077809fe3981c35aaebf63a483a9dd9709004fd
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-05-16 15:17:08 +0200

    Update ContentView.swift

--- Unit Conversion/ContentView.swift
+++ Unit Conversion/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputUnit = 0
+    @State private var outputUnit = 0
+    @State private var amount  = ""
+    
+    private let units = ["meters", "kilometers", "feet", "yards"]
+    private let measurements = [UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards]
+    private var result: Double {
+        let selectedInputUnit = Measurement(value: Double(amount) ?? 0, unit: measurements[self.inputUnit])
+        
+        return selectedInputUnit.converted(to: measurements[self.outputUnit]).value
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Input Units Section")) {
+                    
+                   
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(0..<self.units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    TextField("Amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                    
+                }
+                
+                Section(header: Text("Output Units Section")) {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(0..<self.units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    Text("Result: \(self.result, specifier: "%.2f") ")
+                    
+                }
+            }.navigationBarTitle(Text("Unit Conversion"))
+            
+        }
     }
 }
 
